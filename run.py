bce0a86261c7a4d46d892b25df9c5366a969e95a
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-04-19 16:21:41 +0000

    Add function to generate random number between 0 and 50

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
-# import pyfiglet module
+"""
+import pygiglet to style the game title
+"""
+import random
 import pyfiglet
 
 
 gametitle = pyfiglet.figlet_format("Enigma", font="isometric1")
 print(gametitle)
+
+
+def secret_code(max_range):
+    """
+    Generate random 4 number code
+    """
+    secret_code = []
+    for i in range(4):
+        secret_code.append(random.randint(1, max_range))
+    return secret_code
+
+
+enigma_code = secret_code(50)
+print(enigma_code)

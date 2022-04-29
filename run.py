344231517570b17d6e8b56bd9a4f904f4147d08a
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-04-29 11:26:59 +0000

    Fix unused variable error on line 14

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def secret_code(max_range):
     Generate random 4 number code
     """
     secret_code = []
-    for i in range(4):
+    for _ in range(4):
         secret_code.append(random.randint(1, max_range))
     return secret_code
 

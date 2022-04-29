ccdee9309e32b83364afa2cfbd457852dccb2fb0
Art Cuddy <art.cuddy@dmgmedia.ie>
2022-04-29 13:32:39 +0000

    Add import os module and setup funtion to clear terminal

--- run.py
+++ run.py
@@ -1,15 +1,26 @@
-"""
-import packages to add to program
-"""
+""" ENIGMA Game import packages to add to program """
+# import os to help clear terminal
+import os
+
+# import random module
 import random
+
+# import termcolor for adding colour to text
 from termcolor import colored
+
+# import pyfiglet for ascii art
 from pyfiglet import Figlet
 
 
+def clear():
+    """
+    Clear screen for user on replay
+    """
+    os.system("clear")
+
+
 def secret_code(max_range):
-    """
-    Generate random 4 number code
-    """
+    """ Generate random 4 number code """
     secret_code = []
     for _ in range(4):
         secret_code.append(random.randint(1, max_range))
@@ -22,42 +33,54 @@ f = Figlet(font='banner3-D')
 print(colored(f.renderText('ENIGMA'), 'red'))
 
 
-# Enigma game will run below here
-f = Figlet(font='digital')
-print(colored(f.renderText('Can you crack the code'), 'green'))
-print("Guess all 4 numbers to crack the ENIGMA code!\n")
+def welcome():
+    """
+    ENIGMA Title & welcome message
+    """
+    f = Figlet(font='digital')
+    print(colored(f.renderText('Can you crack the code'), 'green'))
+    print("Guess all 4 numbers to crack the ENIGMA code!\n")
 
 
-# Variable to store the correct guesses and attempts
-CORRECT = 0
-ATTEMPTS = 0
+def main():
+    """
+    Run all program functions and methods
+    """
+    welcome()
 
+    # Variable to store the correct guesses and attempts
+    correct = 0
+    attempts = 0
 
-# Variables to store the players guesses
-while CORRECT < 4:
-    attempt1 = int(input("Please guess the first number: "))
-    attempt2 = int(input("Please guess the second number: "))
-    attempt3 = int(input("Please guess the third number: "))
-    attempt4 = int(input("Please guess the fourth number: "))
-    ATTEMPTS += 1
+    # Variables to store the players guesses
+    while correct < 4:
+        attempt1 = int(input("Please guess the first number: "))
+        attempt2 = int(input("Please guess the second number: "))
+        attempt3 = int(input("Please guess the third number: "))
+        attempt4 = int(input("Please guess the fourth number: "))
+        attempts += 1
 
-    if attempt1 == enigma_code[0]:
-        CORRECT += 1
-    if attempt2 == enigma_code[1]:
-        CORRECT += 1
-    if attempt3 == enigma_code[2]:
-        CORRECT += 1
-    if attempt4 == enigma_code[3]:
-        CORRECT += 1
+        if attempt1 == enigma_code[0]:
+            correct += 1
+        if attempt2 == enigma_code[1]:
+            correct += 1
+        if attempt3 == enigma_code[2]:
+            correct += 1
+        if attempt4 == enigma_code[3]:
+            correct += 1
 
-    if CORRECT < 4:
-        print("\nYou cracked " + str(CORRECT) + " numbers correctly\n")
-        CORRECT = 0
-        continue
-    else:
-        if ATTEMPTS == 1:
-            print("\nCongratulations you cracked the Enigma code!!" +
-                  "You did it in " + str(ATTEMPTS) + " try.\n")
+        if correct < 4:
+            print("\nYou cracked " + str(correct) + " numbers correctly\n")
+            correct = 0
+            continue
         else:
-            print("\nCongratulations you cracked the Enigma code!!" +
-                  "You did it in " + str(ATTEMPTS) + " tries.\n")
+            if attempts == 1:
+                print("\nCongratulations you cracked the Enigma code!!" +
+                      "You did it in " + str(attempts) + " try.\n")
+            else:
+                print("\nCongratulations you cracked the Enigma code!!" +
+                      "You did it in " + str(attempts) + " tries.\n")
+
+
+if __name__ == "__main__":
+    main()
